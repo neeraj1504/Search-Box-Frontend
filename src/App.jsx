824e6797198af7f156055a19bff46d1c4a5d6536
neeraj1504 <bindneeraj1504@gmail.com>
2025-04-15 16:48:05 +0530

    added code to fetch suggestion from database and render it on frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import React, { useState } from "react";
 import Searchbox from "./Components/Searchbox.jsx";
 import Suggestion from "./Components/Suggestion.jsx";
-import Transliterator from "./Components/Translitrator.jsx";
+
+
 const App = () => {
-  const[items,setItems]=useState("");
-  
+  const [inputText, setInputText] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
 
   return (
     <div className="flex flex-col items-center justify-start min-h-screen bg-gray-50 pt-10 px-4">
-      <Searchbox items={items} setItems={setItems} />
-      <Suggestion items={items} setItems={setItems} />
-      <Transliterator/>
+      <Searchbox inputText={inputText} setInputText={setInputText} />
+      <Suggestion inputText={inputText} setSuggestions={setSuggestions} />
+      
     </div>
   );
 };
