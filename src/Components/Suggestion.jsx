2ec96b3c7cd6dbd46bb3debd53a03892985a3443
Aman Kumar Singh <singhaman8409@mail.com>
2025-04-25 10:39:48 +0530

    Removed the transliteration part from frontend in the new approach

--- src/Components/Suggestion.jsx
+++ src/Components/Suggestion.jsx
@@ -1,64 +1,60 @@
 import React, { useEffect, useState } from "react";
-import { hindiToEnglishUtil } from "../Utils";
-import { englishToHindiUtil } from "../Utils";
 
 const Suggestion = ({ items, setItems }) => {
 
   const [suggestionsArray, setSuggestionsArray] = useState([]);
-  useEffect(()=>{
-      let prefix=items;
-      let transiteratedPrefix="";
-    if (prefix && prefix.length > 0) {
-      if(prefix[0] in hindiToEnglishUtil.hindiToEnglish){
-        transiteratedPrefix=hindiToEnglishUtil.transliterateToEnglish(prefix);
-      }
-      else{
-        transiteratedPrefix=englishToHindiUtil.transliterateToHindi(prefix);
-      }
+  useEffect(() => {
+    let prefix = items;
+    //To handle the trailing spaces in the query string encodeURIComponent was added
+    if (prefix) {
+      fetch(`http://localhost:8000/api/v1/getWords?prefix=${encodeURIComponent(prefix)}`)
+
+        .then((res) => res.json()
+        ).then((res) => {
+          const newArray = [];
+
+          res.data.map((element) => {
+            newArray.push(element.word);
+            element.transliterations.map((transWord) => {
+              newArray.push(transWord);
+            })
+          });
+          return newArray;
+        }).then((res) => {
+          setSuggestionsArray(res);
+        })
     }
-  
+    else {
+      setSuggestionsArray([]);
+    }
 
-      let queryPrefix=`${prefix},${transiteratedPrefix}`;
-      //TO handle the trailing spaces in the query string
-     fetch(`http://localhost:8000/api/v1/getWords?prefix=${encodeURIComponent(queryPrefix)}`)
-     .then((res)=>res.json()
-     ).then((res)=>{
-        if(items&&transiteratedPrefix.length
-        ){
-          setSuggestionsArray(res.data);
-        }
-        else{
-          //To remove the persisiting suggestions from the div after the input box set to empty
-          setSuggestionsArray([]);
-        }
-     })
+    console.log(prefix);
 
-      console.log(transiteratedPrefix);
-      console.log(prefix);
-      
 
-  },[items])
-  
- 
-//To add the clicked suggestion in the input box
-const handleClick = (e)=>{
-  setItems(e.target.innerHTML);
-}
+  }, [items])
+
+
+  //To add the clicked suggestion in the input box
+  const handleClick = (e) => {
+    setItems(e.target.innerHTML);
+  }
 
 
   return (
     <div className="w-full max-w-md mx-auto h-auto">
       <div className="border border-t-0 border-gray-300 rounded-b-md h-auto overflow-y-auto bg-white shadow-md">
         {suggestionsArray
-          .map((item, index) =>{return (
-            <div
-              onClick={handleClick}
-              key={index}
-              className="px-4 py-2 hover:bg-gray-100 cursor-pointer"
-            >
-              {item.word}
-            </div>
-          )})}
+          .map((item, index) => {
+            return (
+              <div
+                onClick={handleClick}
+                key={index}
+                className="px-4 py-2 hover:bg-gray-100 cursor-pointer"
+              >
+                {item}
+              </div>
+            )
+          })}
       </div>
     </div>
   );
--- src/Utils/index.js
+++
@@ -1,2 +0,0 @@
-export * as hindiToEnglishUtil from "./hindiToEnglishUtil";
-export * as englishToHindiUtil from "./englishToHindiUtil";
